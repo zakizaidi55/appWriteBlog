5e9f1289f19012498ce422c440ad6826561ed3ba
zakizaidi55 <zakizaidi55@gmail.com>
2024-04-10 02:18:03 +0530

    change the logo (temperory)

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+    <div className='w-20'>
+      <img src= "https://i.pinimg.com/474x/00/d2/0a/00d20a7aac615fea61be480078fe5966.jpg"/>
+    </div>
   )
 }
 
